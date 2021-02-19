12113a08d48084b349ed2852411cb5d09e4678e8
Bobby Rahmanda <69358107+bobbyrahmanda13@users.noreply.github.com>
2021-02-20 00:23:48 +0700

    Add files via upload

---
+++ aria2/Aria2 Download Manager.cmd
@@ -0,0 +1,12 @@
+@Echo off
+ECHO Harap Ganti/Sesuaikan Lokasi linkdownload.txt dan Lokasi untuk tempat downloadnya yang ada pada File Batch ini
+ECHO karena jika tidak di ganti akan error
+ECHO ==============================================================
+ECHO Cara nya Klik Kanan File Aria2 Download Manager
+ECHO Lalu Pilih Edit
+ECHO Dan Ganti Lokasi nya Sesuaikan Dengan PC Kalian
+ECHO ==============================================================
+ECHO Silahkan Tekan Enter Jika Sudah Diganti
+PAUSE >>NULL
+notepad C:\Users\Rahman\aria2\linkdownload.txt
+aria2c -s16 -x16 -i d.txt -d C:\Users\Rahman\Downloads
